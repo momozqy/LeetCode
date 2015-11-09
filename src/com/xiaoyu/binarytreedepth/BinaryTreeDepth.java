07c9fb2e8525ef8dcf8039c05a9b706b5684c612
momozqy <532676071@qq.com>
2015-11-09 18:24:52 +0800

    更新试试

--- src/com/xiaoyu/binarytreedepth/BinaryTreeDepth.java
+++ src/com/xiaoyu/binarytreedepth/BinaryTreeDepth.java
@@ -1,13 +1,33 @@
 package com.xiaoyu.binarytreedepth;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class BinaryTreeDepth {
+	public int removeDuplicates(int[] nums) {
+		if (nums == null)
+			return 0;
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < nums.length; i++)
+			set.add(nums[i]);
+		int[] nums2 = new int[set.size()];
+		int o = 0;
+		Iterator<Integer> it = set.iterator();
+		int index = 0;
+		while (it.hasNext()) {
+			o = it.next();
+			nums2[index++] = o;
+		}
+		nums = nums2;
+		return set.size();
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
+		System.out.println(new BinaryTreeDepth().removeDuplicates(new int[] {
+				1, 1, 1, 2, 2, 2, 2, 4, 4, 4 }));
 	}
-
 }
